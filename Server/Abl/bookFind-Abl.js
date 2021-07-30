86d6834b15eaccad71c839a3564b094fd0a2bba2
GoodToBe <lukasbriza@seznam.cz>
2021-07-30 15:28:02 +0200

    find book update (error handling)

--- Server/Abl/bookFind-Abl.js
+++ Server/Abl/bookFind-Abl.js
@@ -7,25 +7,32 @@ async function findBook(req, res, paramName, paramValue){
     //search by _id
     if(paramName == '_id'){
         await Book.find({_id: paramValue},(err, result) => {
-            if (err) {return res.send(err)}
-            return res.json(result);
+            if (err) {return res.json({Error: "ERROR"})} 
+            else if (result.length==0) {return res.json({Error: "ERROR"})} 
+            else { 
+                return res.json(result);
+            }
         })
     }
     //search by name
-    else if(paramName == 'name'){
+    if(paramName == 'name'){
         await Book.find({name: paramValue},(err, result) => {
-            if (err) {return res.send(err)}
-            return res.json(result);
+            if (err) {return res.json({Error: "ERROR"})} 
+            else if (result.length==0) {return res.json({Error: "ERROR"})} 
+            else { 
+                return res.json(result);
+            }
         })
     }
     //search by author - bude array o více
-    else if(paramName == 'author'){
+    if(paramName == 'author'){
         await Book.find({author: paramValue},(err, result) => {
-            if (err) {return res.send(err)}
-            return res.json(result);
+            if (err) {return res.json({Error: "ERROR"})} 
+            else if (result.length==0) {return res.json({Error: "ERROR"})} 
+            else { 
+                return res.json(result);
+            }
         })
-    } else {
-    res.status(500).json({error: "Něco se nezdařilo: funkce findBook."});
     }
 }
 
