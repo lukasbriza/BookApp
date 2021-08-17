5567211546bd888627a7c2050edd4d28271f8813
GoodToBe <lukasbriza@seznam.cz>
2021-08-17 11:47:57 +0200

    book update route update

--- Server/Abl/bookUpdate-Abl.js
+++ Server/Abl/bookUpdate-Abl.js
@@ -1,20 +1,24 @@
 const mongoose = require('mongoose');
-const {Book} = require("../Schemas/mongooseSchema");
+const {User} = require("../Schemas/mongooseSchema");
 
-async function bookUpdate(req, res, query){
-    await Book.updateOne({_id: query.id},{
-        $set: {
-            name: query.name,
-            author: query.author,
-            description: query.description
-        }
-     },(err,result) => {
-         if(err){return res.json({ERROR: err});}
-         res.json({
-            UPDATE:"ok",
-            RESPONSE: result
-        });
-     });
+async function bookUpdate(req, res, query, user){
+    await User.updateOne({"booksOfUsers._id": query.id},
+            {
+                "$set": {
+                    'booksOfUsers.$':{
+                        name: query.name,
+                        author:query.author,
+                        description:query.description
+                    }
+                }
+            },(err,result) => {
+                if (err) {return res.json({ERROR: err});}
+                res.json({
+                    UPDATE:"ok",
+                    RESPONSE: result
+                });
+            }
+    );
 }
 
 module.exports = {
