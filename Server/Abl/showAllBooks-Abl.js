d75d6a923c6ec40a74b1267b91fcee42a09e0945
GoodToBe <lukasbriza@seznam.cz>
2021-08-17 15:16:23 +0200

    show all books route update

--- Server/Abl/showAllBooks-Abl.js
+++ Server/Abl/showAllBooks-Abl.js
@@ -1,18 +1,17 @@
 //DEPENDENCIES//
 const mongoose = require('mongoose');
-const {Book} = require("../Schemas/mongooseSchema");
+const {User} = require("../Schemas/mongooseSchema");
 ////////////////////////////////////////////////////////////////
 //FUNCTIONS//
-async function showAllBooks(req, res){
-    //wait for
-    await Book.find((err, result) => {
-        if (err){return console.error(err);} //error handle
-        if (result.length == 0){
-            return res.json({books:null});
-        } else {
-            res.json(result);
-        }
-    })
+async function showAllBooks(req, res, user){
+    const userName = user;
+    const userProfile = await User.find({"userName": userName});
+    const bookArray = await userProfile[0].booksOfUsers;
+    if (bookArray.length == 0){
+        return res.json({books:null});
+    } else {
+        res.json(bookArray);
+    }
 }
 
 module.exports = {
