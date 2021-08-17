9eda6e62483e5ffd3cc986efb8a29dcee0c18501
GoodToBe <lukasbriza@seznam.cz>
2021-08-17 11:48:56 +0200

    bookSchema nested into userSchema

--- Server/Schemas/mongooseSchema.js
+++ Server/Schemas/mongooseSchema.js
@@ -4,9 +4,19 @@ const bookSchemaDB = new mongoose.Schema({
     name: {type: 'string', required: true},
     author: {type: 'string', required: true},
     description: {type: 'string', required: false},
-})
-const Book = mongoose.model('Book', bookSchemaDB);
+});
+const Book = mongoose.model('Book', bookSchemaDB, 'Users');
 //model je třída se kterou tvořím poté dokumenty
 
+const userSchemaDB = new mongoose.Schema({
+    userName: {type: 'string', required: true},
+    userPassword: {type: 'string', required: true},
+    booksOfUsers: [bookSchemaDB]
+});
+
+const User = mongoose.model('User', userSchemaDB, 'Users');
+
 module.exports = {
-    bookSchemaDB, Book};
+    bookSchemaDB, Book,
+    userSchemaDB, User
+};
