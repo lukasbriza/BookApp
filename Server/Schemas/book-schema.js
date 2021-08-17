a8683bb48428b6c21095d087711e215d5711c356
GoodToBe <lukasbriza@seznam.cz>
2021-08-17 11:48:34 +0200

    user schema added

--- Server/Schemas/book-schema.js
+++ Server/Schemas/book-schema.js
@@ -1,11 +1,28 @@
 let bookSchema ={
     "type":"object",
-    "properties":{
+    "properties" :{
         "name":{"type": "string"},
         "author":{"type": "string"},
         "description":{"type": "string"}
     },
     "required":["name", "author"]
 }
+let bookArray = {
+    "type": "array",
+    "items": bookSchema
+}
 
-module.exports = bookSchema;
+let userSchema = {
+    "type":"object",
+    "properties" :{
+        "userName":{"type": "string"},
+        "userPassword":{"type": "string"},
+        "booksOfUsers": bookArray
+    },
+    "required":["userName", "userPassword"]
+}
+module.exports = {
+    bookSchema,
+    bookArray,
+    userSchema
+};
